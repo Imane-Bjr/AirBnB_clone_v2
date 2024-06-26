6401721dfbca8ddbc2fd9f358e9ff3d4a9913929
Imane-Bjr <boujirimane1@gmail.com>
2024-06-26 16:55:25 -0700

    Fixing the code

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" document document """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ document document """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ document document """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """ document document """
         new = self.value()
         self.assertEqual(type(new.name), str)
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" """
+""" document document """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """ document document """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ document document """
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ document document """
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """ document document """
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """ document document """
         new = self.value()
         self.assertEqual(type(new.text), str)
